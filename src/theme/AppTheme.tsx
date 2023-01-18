77f7fac2db8a00aeb8db8ba1f4d75bb2173fd7e3
monetelab <monetelab@gmail.com>
2023-01-18 18:36:59 +0100

    create AppTheme

---
+++ src/theme/AppTheme.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const MyColors = {
+  primary: "#790ee3",
+  secondary: "#eb9706",
+  background: "#EEEEEE",
+};
+
+export const MyStyles = StyleSheet.create({
+  loading: {
+    position: "absolute",
+    bottom: 0,
+    top: 0,
+    right: 0,
+    left: 0,
+  },
+});
