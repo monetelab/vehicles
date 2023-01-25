cf8b0e0e52420cdf3bf8fff35f7087f3526bc195
monetelab <monetelab@gmail.com>
2023-01-25 09:07:18 +0100

    delete console log

--- src/Presentation/views/VehicleDetail/VehicleDetails.tsx
+++ src/Presentation/views/VehicleDetail/VehicleDetails.tsx
@@ -34,7 +34,6 @@ export const VehicleDetails = ({ navigation, route }: Props) => {
   function outSession() {
     closeUserSession();
   }
-  console.log("IdSelection", IdSelection);
 
   return (
     <SafeAreaView
